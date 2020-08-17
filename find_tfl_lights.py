c21b150cf165e1feb78a698216dddeefab9b1615
Dvora-Tenenboim <ddt9642@gmail.com>
2020-08-17 19:27:32 +0300

    find all tfl, still need to improve the kernel

--- find_tfl_lights.py
+++ find_tfl_lights.py
@@ -4,84 +4,82 @@ try:
     import json
     import glob
     import argparse
-    #import cv2
 
     print("numpy/scipy imports:")
     import numpy as np
     from scipy import signal as sg
-    from scipy import misc
     import scipy.ndimage as ndimage
     from scipy.ndimage.filters import maximum_filter
+    from skimage.feature import peak_local_max
 
     print("PIL imports:")
     from PIL import Image
 
     print("matplotlib imports:")
     import matplotlib.pyplot as plt
-
 except ImportError:
     print("Need to fix the installation")
     raise
 
 print("All imports okay. Yay!")
 
+kernel = np.array([[-0.5, -0.5, -0.5],
+                       [-0.5, -0.5, -0.5],
+                       [-0.5, -0.5, -0.5],
+                       [1, 1, -0.5],
+                       [1, 2, 1],
+                       [1, 1, 1]])
+
+def find_lights(c_image: np.ndarray):
+    c_image = sg.convolve2d(c_image, kernel)
+    coordinates = peak_local_max(c_image, min_distance=20, num_peaks=10)
+    # show_image_and_gt()
+    return coordinates[:, 1], coordinates[:, 0]
+
 
 def find_tfl_lights(c_image: np.ndarray, **kwargs):
-#def find_tfl_lights(image_path, **kwargs):
-    # construct the argument parse and parse the arguments
-    # ap = argparse.ArgumentParser()
-    # ap.add_argument("-i", "--image", help="path to the image file")
-    # ap.add_argument("-r", "--radius", type=int,
-    #                 help="radius of Gaussian blur; must be odd")
-    # args = vars(ap.parse_args())
-    # args["image"] = image_path
-    # args["radius"] = 11
-    # load the image and convert it to grayscale
-    # image = cv2.imread(args["image"])
-    # orig = image.copy()
-    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # perform a naive attempt to find the (x, y) coordinates of
-    # the area of the image with the largest intensity value
-    # (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
-    # cv2.circle(image, maxLoc, 5, (255, 0, 0), 2)
-    # display the results of the naive attempt
-    # cv2.imshow("Naive", image)
-    # apply a Gaussian blur to the image then find the brightest
-    # region
-    # gray = cv2.GaussianBlur(gray, (args["radius"], args["radius"]), 0)
-    # (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
-    # image = orig.copy()
-    # cv2.circle(image, maxLoc, args["radius"], (255, 0, 0), 2)
-    # display the results of our newly improved method
-    # cv2.imshow("Robust", image)
-    # cv2.waitKey(0)
-
     """
     Detect candidates for TFL lights. Use c_image, kwargs and you imagination to implement
     :param c_image: The image itself as np.uint8, shape of (H, W, 3)
     :param kwargs: Whatever config you want to pass in here
     :return: 4-tuple of x_red, y_red, x_green, y_green
     """
-
-    scharr = np.array([[0,0,0,0,0],
-                       [0,1,1,1,0],
-                       [0,1,1,1,0],
-                       [0,1,1,1,0],
-                       [0,0,0,0,0]])
-
-    c_image = c_image/255
-    grad = sg.convolve2d(c_image[:, :, 1], scharr, boundary='symm', mode='same')
-    plt.imshow(grad)
-
-    x = np.arange(-100, 100, 20) + c_image.shape[1] / 2
-    y_red = [c_image.shape[0] / 2 - 120] * len(x)
-    y_green = [c_image.shape[0] / 2 - 100] * len(x)
-    return x, y_red, x, y_green
+    x_red, y_red = find_lights(c_image[:, :, 0])
+    x_green, y_green = find_lights(c_image[:, :, 1])
+    return x_red, y_red, x_green, y_green
 
 
-def show_image(image, fig_num=None):
+
+def show_image_and_gt(image, objs, fig_num=None):
     plt.figure(fig_num).clf()
     plt.imshow(image)
+    labels = set()
+    if objs is not None:
+        for o in objs:
+            poly = np.array(o['polygon'])[list(np.arange(len(o['polygon']))) + [0]]
+            plt.plot(poly[:, 0], poly[:, 1], 'r', label=o['label'])
+            labels.add(o['label'])
+        if len(labels) > 1:
+            plt.legend()
+
+
+def test_find_tfl_lights(image_path, json_path=None, fig_num=None):
+    """
+    Run the attention code
+    """
+    image = np.array(Image.open(image_path))
+    if json_path is None:
+        objects = None
+    else:
+        gt_data = json.load(open(json_path))
+        what = ['traffic light']
+        objects = [o for o in gt_data['objects'] if o['label'] in what]
+
+    show_image_and_gt(image, objects, fig_num)
+
+    red_x, red_y, green_x, green_y = find_tfl_lights(image, some_threshold=42)
+    plt.plot(red_x, red_y, 'ro', color='r', markersize=4)
+    plt.plot(green_x, green_y, 'ro', color='g', markersize=4)
 
 
 def main(argv=None):
@@ -89,14 +87,24 @@ def main(argv=None):
     Consider looping over some images from here, so you can manually exmine the results
     Keep this functionality even after you have all system running, because you sometime want to debug/improve a module
     :param argv: In case you want to programmatically run this"""
-
-    pictures_path = 'pictures'
-    flist = glob.glob(os.path.join(pictures_path, '*_leftImg8bit.png'))
-    for image_path in flist:
-        image = np.array(Image.open(image_path))
-        red_x, red_y, green_x, green_y = find_tfl_lights(image)
-        # show_image(image)
-
+    parser = argparse.ArgumentParser("Test TFL attention mechanism")
+    parser.add_argument('-i', '--image', type=str, help='Path to an image')
+    parser.add_argument("-j", "--json", type=str, help="Path to json GT for comparison")
+    parser.add_argument('-d', '--dir', type=str, help='Directory to scan images in')
+    args = parser.parse_args(argv)
+    default_base = 'pictures'
+    if args.dir is None:
+        args.dir = default_base
+    flist = glob.glob(os.path.join(args.dir, '*_leftImg8bit.png'))
+    for image in flist:
+        json_fn = image.replace('_leftImg8bit.png', '_gtFine_polygons.json')
+        if not os.path.exists(json_fn):
+            json_fn = None
+        test_find_tfl_lights(image, json_fn)
+    if len(flist):
+        print("You should now see some images, with the ground truth marked on them. Close all to quit.")
+    else:
+        print("Bad configuration?? Didn't find any picture to show")
     plt.show(block=True)
 
 
